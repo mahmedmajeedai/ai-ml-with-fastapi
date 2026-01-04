550198989e27e844631e64fcb9321fff1f4814af
Muhammad Ahmed Majeed <mahmedmajeed.ai@gmail.com>
2026-01-04 18:12:28 +0500

    loading json data

--- Introduction/main.py
+++ Introduction/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+#helper function to load json data
+def load_data():
+    with open('Introduction/patient.json', 'r') as f:
+        data = json.load(f)
+    return data
+
 @app.get("/")
 def hello():
-    return {"message": "Hello World"}
+    return {"message": "Patient Management System"}
 
 @app.get("/about")
 def about():
-    return {'message': 'This is the about page'}
+    return {'message': 'Our first patient API'}
