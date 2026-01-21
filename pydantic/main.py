c4afaccf96a787ab585234dfb29ef4ee9a910dd7
Muhammad Ahmed Majeed <mahmedmajeed.ai@gmail.com>
2026-01-22 03:15:40 +0500

    type validation

--- pydantic/main.py
+++ pydantic/main.py
@@ -1,6 +1,19 @@
 from pydantic import BaseModel
 
 class Patient(BaseModel):
+    # Type validation
     name: str
     age: int
+
+def insert_patient_record(mareez: Patient):
+    print(mareez.name)
+    print(mareez.age)
+    print(type(mareez.age))
+    print("Patient record inserted.")
     
+patient_data = {
+    "name": "Ahmed",
+    "age": 23
+}
+patient1 = Patient(**patient_data)
+insert_patient_record(patient1)
