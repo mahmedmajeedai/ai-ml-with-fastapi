68970866062147596250fafde00bddf55a93d9ee
Muhammad Ahmed Majeed <mahmedmajeed.ai@gmail.com>
2026-01-23 16:40:10 +0500

    input data

--- pydantic/main.py
+++ pydantic/main.py
@@ -1,12 +1,31 @@
 from pydantic import BaseModel
-from typing import List, Dict
+from typing import List, Dict, Union
 
 class Patient(BaseModel):
     # Type validation
     name: str
     age: int
     allergies: List[str]
+    symptoms: List[Union[str, int]] #or X:list
     contact_details: Dict[str, str]
+
+
+def data_entry():
+    name = input("Enter patient's name: ")
+    age = input("Enter patient's age: ")
+    allergies = input("Enter patient's allergies (comma-separated): ").split(",")
+    symptoms = input("Enter patient's symptoms (comma-separated, can include numbers): ").split(",")
+    contact_details = {
+        "email": input("Enter patient's email: "),
+        "phone": input("Enter patient's phone: ")
+    }
+    return Patient(
+        name=name,
+        age=age,
+        allergies=allergies,
+        symptoms=symptoms,
+        contact_details=contact_details
+    )
 
 def insert_patient_record(mareez: Patient):
     print(mareez.name)
@@ -26,5 +45,6 @@ patient_data = {
         "phone": "123-456-7890"
     }
 }
-patient1 = Patient(**patient_data)
-insert_patient_record(patient1)
+# patient1 = Patient(**patient_data)
+# insert_patient_record(patient1)
+data_entry()
